39625b69086683a004a4307dd3b3d20015b386c2
lee991 <aalxxss@gmail.com>
2016-12-31 19:10:37 +0800

    updated

--- func.c
+++ func.c
@@ -1,8 +1,9 @@
-#include <string.h>
+include <string.h>
 
 char *getStudentID(char *id)
 {
 	/* Return your student id. */
+	strcpy(id,"1032982"); //copy my student id into string id
 
 	return id;
 }
